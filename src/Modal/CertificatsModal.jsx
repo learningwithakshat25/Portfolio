e70db158a46aa6344b1c87b468fa494f90d24b5a
Akshat Mittal <2004mittalakshat@gmail.com>
2025-05-14 13:02:29 +0530

    changes

--- src/Modal/CertificatsModal.jsx
+++ src/Modal/CertificatsModal.jsx
@@ -11,7 +11,7 @@ const CertificatsModal = (props) => {
     <>
       <div  key={props.item.id} className="box w-full md:w-80 flex flex-col gap-5 p-3 shadow shadow-black rounded-md bg-gray-50 dark:bg-[#171717] dark:text-white cursor-pointer hover:scale-105 ">
         <img onClick={openCloseModal} key={props.item.id} src={props.item.img} alt="" />
-        <button className='bg-[#27E0B3] text-[#000000] rounded-md p-2 w-full font-semibold cursor-pointer hover:scale-105'><a href={props.item.path} download={props.item.name}>Download</a></button>
+        <button className='bg-[#27E0B3] hover:bg-[#8bfcdf] text-[#000000] rounded-md p-2 w-full font-semibold cursor-pointer hover:scale-105'><a href={props.item.path} download={props.item.name}>Download</a></button>
       </div>
       {open && 
       <div onClick={openCloseModal} className="modal inset-0 z-50 fixed w-full h-full bg-transparent backdrop-blur-md flex items-center justify-center p-5">
